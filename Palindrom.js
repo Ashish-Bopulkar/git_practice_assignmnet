0bff0e725b43d135d7a36bd511e31d9967d03474
anshul <anshulagr799@gmail.com>
2023-02-08 14:50:32 +0530

    commiting palindrome file

--- Palindrom.js
+++ Palindrom.js
@@ -1,17 +1,17 @@
 // Problem 2 : Check whether a string is palindrome or not.
 
-let str = "akash";
+let str  =  "naman"  ;
 
-let bag=[];
+let bag=[]  ;
 
-for(let i=str.length-1;i>=0;i--)
+for ( let i=str.length-1 ; i>=0;i-- )
   {
     bag.push(str[i]);
   }
 
-for(let i=0;i<str.length;i++)
+for ( let i=0 ; i<str.length ; i++ )
   {
-    if(str[i]!=bag[i])
+    if( str[i]!=bag[i] )
     {
       console.log("Not Palindrome");
     }
